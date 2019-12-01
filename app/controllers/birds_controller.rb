abc72c044930c3709a2544a83fdeaf2bb24318da
NaKia Whitby <whitby.nakia@gmail.com>
2019-12-01 22:28:21 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,13 +6,11 @@ class BirdsController < ApplicationController
     render json: birds.to_json(except: [:created_at, :updated_at])
 end
 
-  def show
-    bird = Bird.find_by(id: params[:id])
-    render json: {id: bird.id, name: bird.name, species: bird.species }
-    #render json: bird.slice(:id, :name, :species)
-    if bird
-   render json: { id: bird.id, name: bird.name, species: bird.species }
- else
-   render json: { message: 'Bird not found' }
- end
+def show
+bird = Bird.find_by(id: params[:id])
+if bird
+  render json: { id: bird.id, name: bird.name, species: bird.species }
+else
+  render json: { message: 'Bird not found' }
+end
 end
